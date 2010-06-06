6225f3d4026880b8a2e6e1dbd6fec2b25989d3d2
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2010-06-06 03:33:19 -0700

    got the first version of this working

--- lib/redirector.rb
+++ lib/redirector.rb
@@ -4,15 +4,39 @@ class Redirector
 
   @@config = YAML::load(File.open('config.yaml'))
 
+  def self.config
+    @@config
+  end
+
   def get_config(host)
-    return @@config[host] unless @@config[host]
+    return @@config[host] if @@config[host]
     raise "Configuration for #{host} not available"
   end
 
   def call(env)
     host_config = get_config(env['HTTP_HOST'])
-    puts host_config
     [host_config['status'], {'Content-Type' => 'text/html','Location' => host_config['location']}, '302-Found']
   end
 
 end
+
+
+class Debugger
+
+  def initialize(app, debug)
+    @app = app
+    @debug = debug
+  end
+
+  def call(env)
+    if @debug
+      puts '==== Config information ===='
+      puts Redirector.config.inspect
+      puts '============================'
+    end
+    @app.call(env)
+  end
+
+end
+
+
